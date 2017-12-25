074e1ef300b5524e2bd8492057626f14213c4652
rutinva <vasily.rutin@gmail.com>
2017-12-25 12:43:36 +0300

    URLs for Hydra loaded from .properties files. SpringProfile added

--- src/main/java/com/github/omenstudio/weblibrary/AppConfig.java
+++ src/main/java/com/github/omenstudio/weblibrary/AppConfig.java
@@ -2,18 +2,29 @@ package com.github.omenstudio.weblibrary;
 
 
 import com.github.omenstudio.hydra.utils.HydraUrlResolver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy
 @ComponentScan("com.github.omenstudio.hydra.*")
 public class AppConfig {
 
-    static {
-        HydraUrlResolver.setApplicationAddress("http://localhost:8080");
-//        HydraUrlResolver.setApplicationAddress("https://library-service-articles.herokuapp.com");
-        HydraUrlResolver.setApiPath("/api");
-        HydraUrlResolver.setVocabPath("/api/vocab");
+    @Bean
+    public CommandLineRunner registerApplicationPaths(
+            @Value("${hydra.address}")  String url,
+            @Value("${hydra.entrypoint}")  String entrypoint,
+            @Value("${hydra.vocab}")  String vocab
+    ) {
+        return e -> {
+            HydraUrlResolver.setApplicationAddress(url);
+            HydraUrlResolver.setApiPath(entrypoint);
+            HydraUrlResolver.setVocabPath(vocab);
+        };
     }
 
 }
